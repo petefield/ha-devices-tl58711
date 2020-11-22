806477b825680c726266d27f5184f4c069709e91
Pete Field <pete@the-fields.net>
2020-11-22 22:14:40 +0000

    initial commit

---
+++ Controllers/TL59711Controller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TheFields.Iot.Devices.SPI.Contracts;
+
+namespace TL59711.Controllers
+{
+    public class LedState
+    {
+        public int Channel {get; set;}
+        public double Brightness { get; set; }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class LedsController : ControllerBase
+    {
+        private readonly ILogger<LedsController> _logger;
+        private readonly ITL59711 _TL59711;
+
+        public LedsController(ILogger<LedsController> logger, ITL59711 TL59711 )
+        {
+            _logger = logger;
+            _TL59711 = TL59711;
+        }
+
+        [HttpPost("{led}/{brightness}")]
+        public void state(int led, double brightness)
+        {
+            _TL59711.SetBrightness(led, brightness);
+        }
+
+        [HttpPost]
+        public void state(LedState[] states)
+        {
+            foreach (var state in states)
+            {
+                _TL59711.SetBrightness(state.Channel, state.Brightness);
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using TheFields.Iot.Devices.SPI.Contracts;
+using TheFields.Iot.Devices.SPI;
+
+namespace TL59711
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSingleton<ITL59711, TheFields.Iot.Devices.SPI.TL59711>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "LedStrip", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LedStrip v1"));
+       
+     //       app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
